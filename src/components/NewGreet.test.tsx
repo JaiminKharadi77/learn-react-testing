301d549c7961499f3efbb0a5b32dd41d4333cb4a
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-17 20:41:37 +0530

    React Testing Tutorial - 12 - Grouping Tests

--- src/components/NewGreet.test.tsx
+++ src/components/NewGreet.test.tsx
@@ -2,19 +2,20 @@ import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
 import NewGreet from './NewGreet';
 
-// .only will run only this test in this file
-test.only("Greet renders correctly without props", () => {
-    render(<NewGreet />);
-    const textElement = screen.getByText('Hello');
-    expect(textElement).toBeInTheDocument();
-});
+describe('Greet', () => {
+
+    test("renders  correctly without props", () => {
+        render(<NewGreet />);
+        const textElement = screen.getByText('Hello');
+        expect(textElement).toBeInTheDocument();
+    });
 
 
-// .skip will skip  only this test in this file
-test.skip("Greet displays the passed name prop dynamically", () => {
-    const name = "John K"; // Dynamic variable
-    render(<NewGreet name={name} />);
+    test(" displays the passed name prop dynamically", () => {
+        const name = "John K"; // Dynamic variable
+        render(<NewGreet name={name} />);
 
-    const textElement = screen.getByText(`Hello ${name}`); // Use variable in getByText
-    expect(textElement).toBeInTheDocument();
-});
+        const textElement = screen.getByText(`Hello ${name}`); // Use variable in getByText
+        expect(textElement).toBeInTheDocument();
+    });
+})
